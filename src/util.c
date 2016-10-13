9d468c9c8b7b3073d156f508298e6534a5df0db9
Daniel Carvalho <danielccosta63@gmail.com>
2016-10-13 15:24:27 -0300

    Adicionada UTIL.C, size_ext, siz_filename

---
+++ src/util.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <openssl/md5.h>
+#include "../inc/table.h"
+#include "../inc/huffman_tree.h"
+
+int get_extesion_size(char *src_filename){
+	int size = strlen(src_filename);
+	int i, size_ext=0;
+	for (i = size-1; i >= 0; --i, size_ext++){
+		if(src_filename[i] == '.'){
+			return size_ext;
+		}
+	}
+}
+
+int get_name_size(char *src_filename){
+	int size = strlen(src_filename), size_ext=0;
+	size_ext=get_extesion_size(src_filename);
+	return size-size_ext-1;
+}
+
+int main(){
+	
+	
+	
+	return 0;
+}
